2f45a1009d4af152286b149d9b4af876ebb477ad
nick <kuzminov.n.i@gmail.com>
2018-10-24 20:56:12 +0300

    Доб консоль лог и проверку

--- scripts2.js
+++ scripts2.js
@@ -4,14 +4,21 @@ var b = +prompt('Введите "b"', 'b=');
 var c = +prompt('Введите "c"', 'c=');
 var res;
 
-if ((a < b) && (a > c) || (a > b) && (a < c)) {
+console.log(a);
+console.log(b);
+console.log(c);
+
+if(isNaN(a) || isNaN(b) || isNaN(c)) {
+    res = 'Не все введенные значения - числа';
+}
+else if ((a < b) && (a > c) || (a > b) && (a < c)) {
     res = a;
 }
 else if ((b < a) && (b > c) || (b > a) && (b < c)) {
     res = b;
 }
 else if ((a == b) || (a == c) || (b == c)) {
-    alert('Числа не должны быть равны');
+    res='Числа не должны быть равны';
 }
 else {
     res = c;
